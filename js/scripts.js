fba54a174a13ed8d1536c1a4597566291179cb24
proudfeet <simon.d.proudfoot@gmail.com>
2014-06-10 15:28:15 -0400

    some typos fixed, wheres waldo changed (not in this, in a separate folder)

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,6 @@
 		'use strict';
 		// DOM ready, take it away
 		$("nav a[title='contact']").smoothScroll();
-		$("nav a[title='contact']").on("click", function(){
-			$("#contact").addClass("animated").on("animationend webkitAnimationEnd oAnimationEnd", function(){
-				$(this).removeClass("animated");
-			});
-			console.log("lol")
-		});
 	});
 	
 })(jQuery, this);
